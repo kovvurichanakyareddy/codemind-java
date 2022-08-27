84db54631b8118540ba53384ebeb2e07b39fdb47
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-08-27 15:08:39 +0530

    import java.util.*; class Take_Input_And_Print{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String str=sc.nextLine();         System.out.print(str);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Take_Input_And_Print{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.print(str);
+    }
+}
