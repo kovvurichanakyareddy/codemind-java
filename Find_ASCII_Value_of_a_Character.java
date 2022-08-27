4534a689281a2db4b8db45103e90425fb9466786
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-08-27 13:44:27 +0530

    import java.util.*; class Find_ASCII_Value_of_a_Character{     public static void main(String[] args){         Scanner sc= new Scanner(System.in);         char c= sc.next().charAt(0);         int ascii=c;         System.out.print(ascii);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Find_ASCII_Value_of_a_Character{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        char c= sc.next().charAt(0);
+        int ascii=c;
+        System.out.print(ascii);
+    }
+}
