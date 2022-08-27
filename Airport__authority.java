167fc1b2ff8c501a04df7f30d25c3eec5d11bfd1
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-08-27 23:02:27 +0530

    import java.util.*; class Airport_authority{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         int[] arr = new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int t=sc.nextInt();         int c=0;         for(int i=0;i<n;i++)         {             if(arr[i]<=t)             {                 c++;             }             else             {                 c+=2;             }         }         System.out.print(c);     } }

---
+++ Airport__authority.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Airport_authority{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int t=sc.nextInt();
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<=t)
+            {
+                c++;
+            }
+            else
+            {
+                c+=2;
+            }
+        }
+        System.out.print(c);
+    }
+}
