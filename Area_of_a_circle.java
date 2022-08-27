90ae4d1878ae68744e91726afa2d5b9e2115793f
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2022-08-27 13:25:36 +0530

    import java.util.*; class Area_of_a_circle{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         float f= sc.nextFloat();         double res=3.14*f*f;         System.out.printf("%.2f",res);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Area_of_a_circle{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        float f= sc.nextFloat();
+        double res=3.14*f*f;
+        System.out.printf("%.2f",res);
+    }
+}
