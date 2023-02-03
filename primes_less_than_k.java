acfc10f0dd6c6281400d75ce6b483cbf1b8dd976
Chanakya Reddy <98935734+kovvurichanakyareddy@users.noreply.github.com>
2023-02-03 11:22:07 +0530

    import java.util.*; class Primes_greater_than_k{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n=sc.nextInt(),k,c=0,f;         int[] arr = new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         k=sc.nextInt();         for(int i=0;i<n;i++)         {             if(arr[i]<=k)             {                 f=0;                 for(int j=1;j<=arr[i];j++)                 {                     if(arr[i]%j==0)                     {                         f++;                     }                 }                 if(f==2)                 {                     c++;                 }             }         }         System.out.print(c);     } }

---
+++ primes_less_than_k.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Primes_greater_than_k{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt(),k,c=0,f;
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<=k)
+            {
+                f=0;
+                for(int j=1;j<=arr[i];j++)
+                {
+                    if(arr[i]%j==0)
+                    {
+                        f++;
+                    }
+                }
+                if(f==2)
+                {
+                    c++;
+                }
+            }
+        }
+        System.out.print(c);
+    }
+}
